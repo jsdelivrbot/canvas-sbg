f7043c9552142d38192633190e5c9362af8f14bf
Ben Taitelbaum <ben@coshx.com>
2017-10-10 15:04:06 -0400

    adds shim to load the script from github
     - so we do not have to keep uploading a new global js to the theme

---
+++ load_script.js
@@ -0,0 +1,7 @@
+$(function() {
+    var script = "https://rawgit.com/coshx/canvas-sbg/master/sbg.js";
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = script;
+    $("head").append(s);
+});
