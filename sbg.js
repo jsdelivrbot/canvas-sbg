f8b44a73c7b0bc694b1f8036935e66eda352f01c
Eric Glassman <egman24@gmail.com>
2017-11-02 11:34:02 -0400

    test run on both pages

--- sbg.js
+++ sbg.js
@@ -1,8 +1,23 @@
 $(function() {
   console.log("In sbg.js");
-  var match = location.href.match(/\/courses\/(\d+)\/gradebook#tab-outcome/);
-  if (match) {
-    var course_id = match[1];
+  
+  var matchOutcome = location.href.match(/\/courses\/(\d+)\/gradebook#tab-outcome/);
+  var matchAssignment = location.href.match(/\/courses\/(\d+)\/gradebook#tab-assignment/);
+
+  if (matchOutcome) {
+    var course_id = matchOutcome[1];
+    console.log("course_id: " + course_id);
+    $.get("/api/v1/courses/"+course_id+"/outcome_results")
+      .done(function(data) {
+        console.log("Got data for course: " + course_id);
+        console.dir(data);
+      })
+      .error(function(err) {
+        console.log("An error occurred trying to get outcome results for course: " + course_id);
+        console.dir(err);
+      });
+  } else if(matchAssignment) {
+    var course_id = matchAssignment[1];
     console.log("course_id: " + course_id);
     $.get("/api/v1/courses/"+course_id+"/outcome_results")
       .done(function(data) {
@@ -16,4 +31,5 @@ $(function() {
   } else {
     console.log("not on gradebook outcome mastery tab");
   }
+
 });
