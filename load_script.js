1c54000beeb77b452b679f0ce348c748887ca71a
Eric Glassman <egman24@gmail.com>
2017-11-06 17:21:44 -0500

    add libs

--- load_script.js
+++ load_script.js
@@ -1,7 +1,16 @@
-$(function() {
-    var script = "https://rawgit.com/coshx/canvas-sbg/master/sbg.js";
+var loadScript = function(script){
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.src = script;
     $("head").append(s);
+}
+
+$(function() {
+  [
+   "https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min.js",
+   "https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.5.2/Rx.min.js",
+   "https://rawgit.com/coshx/canvas-sbg/master/sbg.js"
+  ].forEach(loadScript)
+    
+
 });
