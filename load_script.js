527692c017ba19cce46218fad526d25e310c441f
Eric Glassman <egman24@gmail.com>
2017-11-06 17:26:56 -0500

    add RXDOM for ajax

--- load_script.js
+++ load_script.js
@@ -9,6 +9,7 @@ $(function() {
   [
    "https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min.js",
    "https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.5.2/Rx.min.js",
+   "https://cdnjs.cloudflare.com/ajax/libs/rxjs-dom/7.0.3/rx.dom.min.js",
    "https://rawgit.com/coshx/canvas-sbg/master/sbg.js"
   ].forEach(loadScript)
     
