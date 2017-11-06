96a473972ae3b115d6c5b3de91cc571d54f663aa
manglass <egman24@gmail.com>
2017-11-06 17:46:18 -0500

    refactor

--- sbg.js
+++ sbg.js
@@ -4,7 +4,10 @@ $(function() {
   var matchOutcome = location.href.match(/\/courses\/(\d+)\/gradebook#tab-outcome/);
   var matchAssignment = location.href.match(/\/courses\/(\d+)\/gradebook#tab-assignment/);
 
-  if (matchOutcome) {
+  if (matchAssignment) {
+
+  	var course_id = matchAssignment[1];
+    console.log("course_id: " + course_id);
 
 	  [
 	   "https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min.js",
@@ -12,39 +15,21 @@ $(function() {
 	   "https://cdnjs.cloudflare.com/ajax/libs/axios/0.17.0/axios.min.js"
 	  ].forEach(loadScript)
 
+	  Rx.Observable.fromPromise(axios.get(`/api/v1/courses/${course_id}/outcome_results`))
+      .do(console.log)
+      .subscribe();
 
-    var course_id = matchOutcome[1];
-    console.log("course_id: " + course_id);
-    $.get("/api/v1/courses/"+course_id+"/outcome_results")
-      .done(function(data) {
-        console.log("Got data for course: " + course_id);
-        console.dir(data);
-      })
-      .error(function(err) {
-        console.log("An error occurred trying to get outcome results for course: " + course_id);
-        console.dir(err);
-      });
-  } else if(matchAssignment) {
+    // $.get("/api/v1/courses/"+course_id+"/outcome_results")
+    //   .done(function(data) {
+    //     console.log("Got data for course: " + course_id);
+    //     console.dir(data);
+    //   })
+    //   .error(function(err) {
+    //     console.log("An error occurred trying to get outcome results for course: " + course_id);
+    //     console.dir(err);
+    //   });
+    // console.log("not on gradebook outcome mastery tab");
 
-	  [
-	   "https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min.js",
-	   "https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.5.2/Rx.min.js",
-	   "https://cdnjs.cloudflare.com/ajax/libs/axios/0.17.0/axios.min.js"
-	  ].forEach(loadScript)
-
-    var course_id = matchAssignment[1];
-    console.log("course_id: " + course_id);
-    $.get("/api/v1/courses/"+course_id+"/outcome_results")
-      .done(function(data) {
-        console.log("Got data for course: " + course_id);
-        console.dir(data);
-      })
-      .error(function(err) {
-        console.log("An error occurred trying to get outcome results for course: " + course_id);
-        console.dir(err);
-      });
-  } else {
-    console.log("not on gradebook outcome mastery tab");
   }
 
 });
