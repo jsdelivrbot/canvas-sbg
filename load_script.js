674bcade35e851bb371c06ee0792fc9e8c9375ed
manglass <egman24@gmail.com>
2017-11-06 17:53:11 -0500

    fixes

--- load_script.js
+++ load_script.js
@@ -1,8 +1,16 @@
-var loadScript = function(script){
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = script;
-    $("head").append(s);
+var loadScript = function(src, callback) {
+  var script = document.createElement('script'),
+      loaded;
+  script.setAttribute('src', src);
+  if (callback) {
+    script.onreadystatechange = script.onload = function() {
+      if (!loaded) {
+        callback();
+      }
+      loaded = true;
+    };
+  }
+  document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 $(function() { loadScript("https://rawgit.com/coshx/canvas-sbg/master/sbg.js") });
--- sbg.js
+++ sbg.js
@@ -1,3 +1,22 @@
+var loadSbg = function(){
+
+  Rx.Observable.fromPromise(axios.get(`/api/v1/courses/${course_id}/outcome_results`))
+    .do(console.log)
+    .subscribe();
+
+  // $.get("/api/v1/courses/"+course_id+"/outcome_results")
+  //   .done(function(data) {
+  //     console.log("Got data for course: " + course_id);
+  //     console.dir(data);
+  //   })
+  //   .error(function(err) {
+  //     console.log("An error occurred trying to get outcome results for course: " + course_id);
+  //     console.dir(err);
+  //   });
+  // console.log("not on gradebook outcome mastery tab");
+
+}
+
 $(function() {
   console.log("In sbg.js");
   
@@ -9,26 +28,13 @@ $(function() {
   	var course_id = matchAssignment[1];
     console.log("course_id: " + course_id);
 
-	  [
-	   "https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min.js",
-	   "https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.5.2/Rx.min.js",
-	   "https://cdnjs.cloudflare.com/ajax/libs/axios/0.17.0/axios.min.js"
-	  ].forEach(loadScript)
-
-	  Rx.Observable.fromPromise(axios.get(`/api/v1/courses/${course_id}/outcome_results`))
-      .do(console.log)
-      .subscribe();
-
-    // $.get("/api/v1/courses/"+course_id+"/outcome_results")
-    //   .done(function(data) {
-    //     console.log("Got data for course: " + course_id);
-    //     console.dir(data);
-    //   })
-    //   .error(function(err) {
-    //     console.log("An error occurred trying to get outcome results for course: " + course_id);
-    //     console.dir(err);
-    //   });
-    // console.log("not on gradebook outcome mastery tab");
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min.js", function(){
+    	loadScript("https://cdnjs.cloudflare.com/ajax/libs/rxjs/5.5.2/Rx.min.js", function(){
+    		loadScript("https://cdnjs.cloudflare.com/ajax/libs/axios/0.17.0/axios.min.js", function(){
+          loadSbg()
+    		})
+    	})
+    })
 
   }
 
